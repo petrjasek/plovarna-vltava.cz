2f25943b31c6579477adb20bdd827356891b047b
Petr Jašek <jasekpetr@gmail.com>
2012-02-05 18:13:59 +0100

    add logos, scrolling

---
+++ static/script.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('nav a').click(function(e) {
+        e.preventDefault();
+        var target = $($(this).attr('href')).first();
+        $('html, body').animate({scrollTop: target.position().top - 25}, 3000);
+    });
+
+    $(window).scroll(function(e) {
+        $('nav a.active').removeClass('active');
+        if ($(window).scrollTop() < $('#vize').position().top - 25) {
+            $('nav a:first').addClass('active');
+            return;
+        }
+
+        var sections = $('section');
+        for (var i = 0; i < sections.size(); i++) {
+            if ($(sections[i]).position().top - 25 > $(window).scrollTop()) {
+                if (i > 0) {
+                    var section = sections[i-1];
+                } else {
+                    var section = sections[i];
+                }
+
+                $('nav a[href="#' + $(section).attr('id') + '"]').addClass('active');
+                return;
+            }
+        }
+        sections.first().addClass('active');
+    });
+});
